1e7c8615264728019d4bbc1a4a1ffb1669e427d1
dang_ya_ming <dangyaming@outlook.com>
2023-08-01 14:13:06 +0800

    bugfix

--- src/modules/notice/notice.service.ts
+++ src/modules/notice/notice.service.ts
@@ -5,28 +5,17 @@ import { UserEntity } from '../user/entities';
 
 import { NoticeEntity } from './entities/notice.entity';
 import { QueryDto } from './notice.dto';
-import { NoticeTypes } from './types';
 
 @Injectable()
 export class NoticeService {
     async paginate(user: UserEntity, queryDto: QueryDto): Promise<PaginateReturn<NoticeEntity>> {
-        const query = NoticeEntity.createQueryBuilder('notice').leftJoinAndSelect(
-            'notice.user',
-            'notice.operator',
-        );
-        switch (queryDto.type) {
-            case NoticeTypes.COLLECT:
-            case NoticeTypes.LIKE:
-                query.leftJoinAndSelect('notice.post', 'post');
-                break;
-            case NoticeTypes.COMMENT:
-                query.leftJoinAndSelect('notice.comment', 'comment');
-                break;
-            default:
-                break;
-        }
-        query
+        const query = NoticeEntity.createQueryBuilder('notice')
+            .leftJoinAndSelect('notice.user', 'user')
+            .leftJoinAndSelect('notice.operator', 'operator')
+            .leftJoinAndSelect('notice.post', 'post')
+            .leftJoinAndSelect('notice.comment', 'comment')
             .where('notice.userId = :userId', { userId: user.id })
+            .andWhere('notice.type = :type', { type: queryDto.type })
             .offset(queryDto.limit * (queryDto.page - 1))
             .limit(queryDto.limit);
         const data = await query.getManyAndCount();
